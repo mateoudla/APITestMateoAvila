5a4a9e5e6e9677e9f07cccc407f1c8b18b62de27
mateoudla <117088528+mateoudla@users.noreply.github.com>
2023-02-01 08:48:20 -0500

    Clase UserMA y APIClientMA

---
+++ APITestMateoAvila/Models/UsersMA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace APITestMateoAvila.Models
+{
+    public class UsersMA
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+    }
+
+}
+
---
+++ APITestMateoAvila/ViewModels/APIClientMA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using APITestMateoAvila.Models;
+
+namespace APITestMateoAvila.ViewModels
+{
+    public class APIClientma
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<List<UsersMA>> GetUsers()
+        {
+            var response = await client.GetAsync("https://reqres.in/api/users");
+            response.EnsureSuccessStatusCode();
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<List<UsersMA>>(responseStream);
+        }
+    }
+}
+
+
